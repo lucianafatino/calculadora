506702880bc3f892447a01aeac68498fbf00fe64
Luciana Venancio <lucianavenancio54@gmail.com>
2021-11-09 19:49:55 -0300

    Calculadora

---
+++ main.js
@@ -0,0 +1,19 @@
+var primeiroValor = parseInt(prompt('Digite o primeiro valor: '));
+var segundoValor = parseInt(prompt('Digite o segundo valor: '));
+var operacao = prompt('Escolha a operação! Digite 1 para fazer a divisão, 2 para multiplicação, 3 para subtração ou 4 para adição: ');
+
+if(operacao == 1){
+  var resultado = primeiroValor / segundoValor;
+  document.write('<h2>' + primeiroValor + ' / ' + segundoValor + ' = ' + resultado + '</h2>');
+} else if(operacao == 2){
+  var resultado = primeiroValor * segundoValor;
+  document.write('<h2>' + primeiroValor + ' x ' + segundoValor + ' = ' + resultado + '</h2>');
+} else if(operacao == 3){
+  var resultado = primeiroValor - segundoValor;
+  document.write('<h2>' + primeiroValor + ' - ' + segundoValor + ' = ' + resultado + '</h2>');
+} else if(operacao == 4){
+  var resultado = primeiroValor + segundoValor;
+  document.write('<h2>' + primeiroValor + ' + ' + segundoValor + ' = ' + resultado + '</h2>');
+} else {
+  document.write('<h2>Opção inválida!</h2>');
+}
